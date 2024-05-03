63b960c0e76ae28084dce88495dd285e4d349577
XiandaDu <283149519du@gmail.com>
2024-05-02 20:38:22 -0400

    feat: decision tree finished

--- 4.DecisionTree/DecisionTree.py
+++ 4.DecisionTree/DecisionTree.py
@@ -5,7 +5,8 @@ import operator
 
 
 def create_data_set():
-    data_set = [[0, 0, 0, 0, 'no'],
+    # Create a sample dataset with features and labels
+    dataset = [[0, 0, 0, 0, 'no'],
                 [0, 0, 0, 1, 'no'],
                 [0, 1, 0, 1, 'yes'],
                 [0, 1, 1, 0, 'yes'],
@@ -20,16 +21,19 @@ def create_data_set():
                 [2, 1, 0, 1, 'yes'],
                 [2, 1, 0, 2, 'yes'],
                 [2, 0, 0, 0, 'no']]
-    labels = ['F1-AGE', 'F2-WORK', 'F3-HOME', 'F4-LOAN']
-    return data_set, labels
+    labels = ['F1-AGE', 'F2-WORK', 'F3-HOME', 'F4-LOAN']  # Features labels
+    return dataset, labels
 
 
 def create_tree(dataset, labels, feat_labels):
-    classList = [example[-1] for example in dataset]
-    if classList.count(classList[0]) == len(classList):
-        return classList[0]
+    class_list = [example[-1] for example in dataset]
+    # If all classes in the dataset are the same, return the class label
+    if class_list.count(class_list[0]) == len(class_list):
+        return class_list[0]
+    # If there is only one feature left, return the majority class label
     if len(dataset[0]) == 1:
-        return majority_count(classList)
+        return majority_count(class_list)
+    # Choose the best feature to split the dataset
     best_feat = choose_best_feature_to_split(dataset)
     best_feat_label = labels[best_feat]
     feat_labels.append(best_feat_label)
@@ -37,26 +41,31 @@ def create_tree(dataset, labels, feat_labels):
     del labels[best_feat]
     feat_value = [example[best_feat] for example in dataset]
     unique_vals = set(feat_value)
+    # Recursively create tree for each unique value of the best feature
     for value in unique_vals:
         sublabels = labels[:]
         my_tree[best_feat_label][value] = create_tree(split_data_set(dataset, best_feat, value), sublabels, feat_labels)
     return my_tree
 
 
-def majority_count(classList):
+def majority_count(class_list):
+    # Count the occurrences of each class label
     class_count = {}
-    for vote in classList:
-        if vote not in class_count.keys(): class_count[vote] = 0
+    for vote in class_list:
+        if vote not in class_count.keys():
+            class_count[vote] = 0
         class_count[vote] += 1
+    # Sort the class labels by their counts in descending order
     sortedclass_count = sorted(class_count.items(), key=operator.itemgetter(1), reverse=True)
-    return sortedclass_count[0][0]
+    return sortedclass_count[0][0]  # Return the class label with the highest count
 
 
 def choose_best_feature_to_split(dataset):
     num_features = len(dataset[0]) - 1
-    base_entropy = calc_shannon_ent(dataset)
+    base_entropy = calc_shannon_ent(dataset)  # Calculate the base entropy of the dataset
     best_info_gain = 0
     best_feature = -1
+    # Iterate through each feature and calculate its information gain
     for i in range(num_features):
         feat_list = [example[i] for example in dataset]
         unique_vals = set(feat_list)
@@ -69,24 +78,26 @@ def choose_best_feature_to_split(dataset):
         if info_gain > best_info_gain:
             best_info_gain = info_gain
             best_feature = i
-    return best_feature
+    return best_feature  # Return the index of the feature with the highest information gain
 
 
 def split_data_set(dataset, axis, val):
+    # Split dataset based on a particular feature value
     ret_data_set = []
     for featVec in dataset:
         if featVec[axis] == val:
-            reducedFeatVec = featVec[:axis]
-            reducedFeatVec.extend(featVec[axis + 1:])
-            ret_data_set.append(reducedFeatVec)
+            reduced_feat_vec = featVec[:axis]
+            reduced_feat_vec.extend(featVec[axis + 1:])
+            ret_data_set.append(reduced_feat_vec)
     return ret_data_set
 
 
 def calc_shannon_ent(dataset):
+    # Calculate Shannon entropy of a dataset
     num_examples = len(dataset)
     label_counts = {}
-    for featVec in dataset:
-        current_label = featVec[-1]
+    for feat_vec in dataset:
+        current_label = feat_vec[-1]
         if current_label not in label_counts.keys():
             label_counts[current_label] = 0
         label_counts[current_label] += 1
@@ -98,6 +109,7 @@ def calc_shannon_ent(dataset):
 
 
 def get_num_leafs(my_tree):
+    # Get the number of leaf nodes in a decision tree
     num_leafs = 0
     first_str = next(iter(my_tree))
     second_dict = my_tree[first_str]
@@ -110,6 +122,7 @@ def get_num_leafs(my_tree):
 
 
 def getTreeDepth(my_tree):
+    # Get the depth of a decision tree
     max_depth = 0
     first_str = next(iter(my_tree))
     second_dict = my_tree[first_str]
@@ -118,55 +131,58 @@ def getTreeDepth(my_tree):
             this_depth = 1 + getTreeDepth(second_dict[key])
         else:
             this_depth = 1
-        if this_depth > max_depth: max_depth = this_depth
+        if this_depth > max_depth:
+            max_depth = this_depth
     return max_depth
 
 
 def plot_node(node_txt, center_pt, parent_pt, node_type):
+    # Plot a node with annotation
     arrow_args = dict(arrowstyle="<-")
-    font = FontProperties(fname=r"c:\windows\fonts\simsunb.ttf", size=14)
     create_plot.ax1.annotate(node_txt, xy=parent_pt, xycoords='axes fraction',
                              xytext=center_pt, textcoords='axes fraction',
-                             va="center", ha="center", bbox=node_type, arrowprops=arrow_args, FontProperties=font)
+                             va="center", ha="center", bbox=node_type, arrowprops=arrow_args)
 
 
-def plot_mid_text(cntrPt, parentPt, txtString):
-    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
-    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
-    create_plot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+def plot_mid_text(cntrPt, parentPt, txt_string):
+    # Plot text between two points
+    x_mid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
+    y_mid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    create_plot.ax1.text(x_mid, y_mid, txt_string, va="center", ha="center", rotation=30)
 
 
-def plot_tree(myTree, parentPt, nodeTxt):
-    decisionNode = dict(boxstyle="sawtooth", fc="0.8")
-    leafNode = dict(boxstyle="round4", fc="0.8")
-    numLeafs = get_num_leafs(myTree)
-    depth = getTreeDepth(myTree)
-    firstStr = next(iter(myTree))
-    cntrPt = (plot_tree.xOff + (1.0 + float(numLeafs)) / 2.0 / plot_tree.totalW, plot_tree.yOff)
-    plot_mid_text(cntrPt, parentPt, nodeTxt)
-    plot_node(firstStr, cntrPt, parentPt, decisionNode)
-    secondDict = myTree[firstStr]
+def plot_tree(my_tree, parentPt, node_txt):
+    # Plot the decision tree
+    decision_node = dict(boxstyle="sawtooth", fc="0.8")  # Decision node style
+    leaf_node = dict(boxstyle="round4", fc="0.8")  # Leaf node style
+    num_leafs = get_num_leafs(my_tree)
+    firstStr = next(iter(my_tree))
+    cntrPt = (plot_tree.xOff + (1.0 + float(num_leafs)) / 2.0 / plot_tree.totalW, plot_tree.yOff)
+    plot_mid_text(cntrPt, parentPt, node_txt)
+    plot_node(firstStr, cntrPt, parentPt, decision_node)
+    second_dict = my_tree[firstStr]
     plot_tree.yOff = plot_tree.yOff - 1.0 / plot_tree.totalD
-    for key in secondDict.keys():
-        if type(secondDict[key]).__name__ == 'dict':
-            plot_tree(secondDict[key], cntrPt, str(key))
+    for key in second_dict.keys():
+        if type(second_dict[key]).__name__ == 'dict':
+            plot_tree(second_dict[key], cntrPt, str(key))
         else:
             plot_tree.xOff = plot_tree.xOff + 1.0 / plot_tree.totalW
-            plot_node(secondDict[key], (plot_tree.xOff, plot_tree.yOff), cntrPt, leafNode)
+            plot_node(second_dict[key], (plot_tree.xOff, plot_tree.yOff), cntrPt, leaf_node)
             plot_mid_text((plot_tree.xOff, plot_tree.yOff), cntrPt, str(key))
     plot_tree.yOff = plot_tree.yOff + 1.0 / plot_tree.totalD
 
 
 def create_plot(inTree):
-    fig = plt.figure(1, facecolor='white')  # 创建fig
-    fig.clf()  # 清空fig
+    # Create a plot for the decision tree
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
     axprops = dict(xticks=[], yticks=[])
-    create_plot.ax1 = plt.subplot(111, frameon=False, **axprops)  # 去掉x、y轴
-    plot_tree.totalW = float(get_num_leafs(inTree))  # 获取决策树叶结点数目
-    plot_tree.totalD = float(getTreeDepth(inTree))  # 获取决策树层数
+    create_plot.ax1 = plt.subplot(111, frameon=False, **axprops)  # Remove x and y axis
+    plot_tree.totalW = float(get_num_leafs(inTree))  # Get the total number of leaf nodes
+    plot_tree.totalD = float(getTreeDepth(inTree))  # Get the total depth of the tree
     plot_tree.xOff = -0.5 / plot_tree.totalW
-    plot_tree.yOff = 1.0  # x偏移
-    plot_tree(inTree, (0.5, 1.0), '')  # 绘制决策树
+    plot_tree.yOff = 1.0  # x offset
+    plot_tree(inTree, (0.5, 1.0), '')  # Plot the decision tree
     plt.show()
 
 
